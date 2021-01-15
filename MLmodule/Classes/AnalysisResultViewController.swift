6ba64a811ca6eb03d967b0c55e41f9371ce7636a
Lleitao1 <lucas.leitao25@gmail.com>
2021-01-15 10:23:15 -0300

    mudar target analyzer

--- MLmodule/Classes/AnalysisResultViewController.swift
+++ MLmodule/Classes/AnalysisResultViewController.swift
@@ -16,7 +16,7 @@ public class AnalysisResultViewController: UIViewController {
     public init(image: UIImage, results: [VNClassificationObservation]) {
         self.imageToLoad = image
         self.results = results
-        super.init(nibName: "AnalysisResultViewController", bundle: nil)
+        super.init(nibName: "AnalysisResultViewController", bundle: Bundle(for: AnalysisResultViewController.self)) 
     }
     
     required init?(coder: NSCoder) {
